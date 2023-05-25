bf4cb9fb2ea0f0faf34dc6d4a30dd20e3a91caaf
dasfef <mealhouse3377@gmail.com>
2023-05-25 17:20:36 +0900

    Add files day06(3)

--- day06/app/src/main/java/com/example/day06/MainActivity.java
+++ day06/app/src/main/java/com/example/day06/MainActivity.java
@@ -6,20 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup rdoGroup;
+    RadioButton rdo1, rdo2;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btnNew = findViewById(R.id.btnNewActivity);
+        rdoGroup = findViewById(R.id.rdoGroup);
+        rdo1 = findViewById(R.id.rdo1);
+        rdo2 = findViewById(R.id.rdo2);
+
+        rdoGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch(rdoGroup){
+
+                }
+            }
+        });
+
         btnNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 startActivity(intent);
+
             }
         });
     }
---
+++ day06_makeDB/app/src/main/java/com/example/day06_makedb/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.day06_makedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    myDBHelper myHelper;
+    EditText editName, editNumber, editNameResult, editNumberResult;
+    Button btnInit, btnInsert, btnSelect;
+    SQLiteDatabase sqlDB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("가수 그룹 관리 DB");
+
+        editName = (EditText) findViewById(R.id.edtName);
+        editNumber = (EditText) findViewById(R.id.edtNumber);
+        editNameResult = (EditText) findViewById(R.id.edtNameResult);
+        editNumberResult = (EditText) findViewById(R.id.edtNumberResult);
+
+        btnInit = (Button) findViewById(R.id.btnInit);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB, 1, 2);
+                sqlDB.close();
+            }
+        });
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO groupTBL VALUES('" + editName.getText().toString() + "', " + editNumber.getText().toString() + ")");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
+                editName.setText("");
+                editNumber.setText("");
+            }
+        });
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("SELECT * FROM groupTBL;", null);
+
+                String strNames = "그룹 이름" + "\r\n" + "----------" + "\r\n";
+                String strNumbers = "인원" + "\r\n" + "----------" + "\r\n";
+
+                while(cursor.moveToNext()){
+                    strNames += cursor.getString(0) + "\r\n";
+                    strNumbers += cursor.getString(1) + "\r\n";
+                }
+
+                editNameResult.setText(strNames);
+                editNumberResult.setText(strNumbers);
+
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context){
+            super(context, "groupDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table groupTBL (gName char(20) PRIMARY KEY, gNumber INTEGER);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS groupTBL;");
+            onCreate(db);
+        }
+    }
+}
