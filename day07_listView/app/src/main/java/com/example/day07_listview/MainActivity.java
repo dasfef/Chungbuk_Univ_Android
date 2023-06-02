b5ec4a56d653e4c72b78e82b3f2f10d23140722b
dasfef <mealhouse3377@gmail.com>
2023-06-02 18:01:20 +0900

    Add data communication by MySQL

---
+++ day07_listView/app/src/main/java/com/example/day07_listview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.day07_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("리스트뷰 테스트");
+
+        String[] mid = {"히어로즈", "24시", "로스트", "로스트룸", "스몰빌", "탐정몽크", "빅뱅이론", "프렌즈", "덱스터", "글리", "가십걸", "테이큰", "슈퍼내추럴", "브이"};
+
+        ListView list = (ListView) findViewById(R.id.listView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mid);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Toast.makeText(getApplicationContext(), mid[arg2], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
